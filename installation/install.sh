bfebe31fd3ab67e2192e6aa6cc69913d9c79290d
Chun-da Chen <capitalm.c@gmail.com>
2017-09-16 15:21:28 +0800

    Fix installation.
    - Use debian-based PHP to prevent `GLOB_BRACE` error.
    - Detect UID of user `www-data`.

--- installation/install.sh
+++ installation/install.sh
@@ -19,6 +19,12 @@ OPENCART_FILE="opencart-tw-2.3.0.2-20170116.zip"
 DOCUMENT_ROOT="/var/www/html"
 INSTALL_ROOT="/installation_data"
 
+waitforservice web:80
+waitforservice php:9000
+waitforservice mysql:3306
+
+PHP_UID=`cat /var/www/html/uid`
+
 cd "$INSTALL_ROOT"
 wget -N "$OPENCART_URL/$OPENCART_FILE" || exit 1
 chmod 666 "$OPENCART_FILE"
@@ -29,13 +35,9 @@ unzip -u "$INSTALL_ROOT/$OPENCART_FILE" || exit 1
 cp /var/www/html/public/upload/config-dist.php /var/www/html/public/upload/config.php
 cp /var/www/html/public/upload/admin/config-dist.php /var/www/html/public/upload/admin/config.php
 
-chown -R 82 /var/www/html/public
+chown -R "$PHP_UID" /var/www/html/public
 
-waitforservice web:80
-waitforservice php:9000
-waitforservice mysql:3306
-
-curl -D - -X POST \
+curl -sSL -X POST \
   -H "Host: ${SERVER_HOSTNAME}" \
   -F "db_driver=mpdo" \
   -F "db_hostname=mysql" \
